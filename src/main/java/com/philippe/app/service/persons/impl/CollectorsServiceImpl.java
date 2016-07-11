d68f8b9b0838ad6018e1a19e1721bf1a2ad51bc1
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-07-11 16:39:53 +0100

    displaying stats on ge using Collectors

--- src/main/java/com/philippe/app/service/persons/impl/CollectorsServiceImpl.java
+++ src/main/java/com/philippe/app/service/persons/impl/CollectorsServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Named;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -36,5 +37,9 @@ public class CollectorsServiceImpl implements CollectorsService {
 
     Double averageAge = personsList.stream().collect(Collectors.averagingInt(p -> p.getAge()));
     log.debug("averageAge = {}", averageAge);
+
+    // Displays ageSummary = IntSummaryStatistics{count=4, sum=76, min=12, average=19.000000, max=23}
+    IntSummaryStatistics ageSummary = personsList.stream().collect(Collectors.summarizingInt(p -> p.getAge()));
+    log.debug("ageSummary = {}", ageSummary);
   }
 }
