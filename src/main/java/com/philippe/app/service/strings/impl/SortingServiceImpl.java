917b72809f66a440bc59d884fa88fa62f05904f1
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-07-11 11:06:53 +0100

    manipulating streams

--- src/main/java/com/philippe/app/service/strings/impl/SortingServiceImpl.java
+++ src/main/java/com/philippe/app/service/strings/impl/SortingServiceImpl.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Named;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @Named
@@ -16,5 +18,17 @@ public class SortingServiceImpl implements SortingService {
         .map(String::toUpperCase)
         .sorted()
         .forEach(log::debug);
+
+    // transform a regular object stream to a primitive stream
+    Stream.of("a1", "a2", "a3")
+        .map(s -> s.substring(1))
+        .mapToInt(Integer::parseInt)
+        .max()
+        .ifPresent(System.out::println);
+
+    // transform a primitive stream to a regular object stream
+    IntStream.range(1, 4)
+        .mapToObj(i -> "a" + i)
+        .forEach(System.out::println);
   }
 }
