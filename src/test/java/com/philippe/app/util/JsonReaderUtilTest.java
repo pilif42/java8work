e8b471acf1431e612e6f6fa6c518ab20d0351095
Philippe Brossier <brossierp@gmail.com>
2018-10-22 21:51:24 +0100

    test to beautify json

--- src/test/java/com/philippe/app/util/JsonReaderUtilTest.java
+++ src/test/java/com/philippe/app/util/JsonReaderUtilTest.java
@@ -2,11 +2,22 @@ package com.philippe.app.util;
 
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import static com.philippe.app.util.JsonReaderUtil.beautifyJsonString;
+import static org.junit.Assert.assertEquals;
 
 public class JsonReaderUtilTest {
+
+
+    private static final String JSON_FILE_PATH = "src/test/resources/json/sample.json";
+
     @Test
     public void testBeautifyJsonString() throws Exception {
-        beautifyJsonString("{\"menu\":{\"id\":\"file\",\"value\":\"File\",\"popup\":{\"menuitem\":[{\"value\":\"New\",\"onclick\":\"CreateNewDoc()\"},{\"value\":\"Open\",\"onclick\":\"OpenDoc()\"},{\"value\":\"Close\",\"onclick\":\"CloseDoc()\"}]}}}");
+        List<String> events = Files.readAllLines(Paths.get(JSON_FILE_PATH).toAbsolutePath());
+        assertEquals(1, events.size());
+        beautifyJsonString(events.get(0));
     }
 }
