3743c8f08cda0276a034ee4a58c2ac499aac1462
Philippe Brossier <brossierp@gmail.com>
2019-03-19 10:12:08 +0000

    TODO left in the code

--- src/main/java/com/philippe/app/service/hbase/HBaseWriteManager.java
+++ src/main/java/com/philippe/app/service/hbase/HBaseWriteManager.java
@@ -3,7 +3,9 @@ package com.philippe.app.service.hbase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +14,23 @@ public class HBaseWriteManager implements Runnable {
     private static final Logger LOGGER = LoggerFactory.getLogger(HBaseWriteManager.class);
 
     private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private LinkedBlockingQueue queue;
+
+    public HBaseWriteManager() {
+        queue = new LinkedBlockingQueue(100);
+    }
+
+    public void write(List<String> putList) {
+        // TODO This write method is the way to pass the HBase Puts to the HBaseWriteManager.
+        // TODO Replace List<String> with List<org.apache.hadoop.hbase.client.Put>
+        // TODO For each Put, queue.put(put)
+    }
 
     @Override
     public void run() {
         LOGGER.debug("Entering run...");
+        // TODO put = queue.take()
+        // TODO this.mutator.mutate(put) where mutator is org.apache.hadoop.hbase.client.BufferedMutator
     }
 
     public void startScheduleTask() {
@@ -29,5 +44,6 @@ public class HBaseWriteManager implements Runnable {
 
     private void flushHBaseCache() {
         LOGGER.debug("Flushing the HBase cache...");
+        // TODO this.mutator.flush
     }
 }
