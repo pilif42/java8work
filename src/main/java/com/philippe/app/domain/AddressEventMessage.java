7da2d7ca40def363cac5b1433435fb6df70c4070
Philippe Brossier <brossierp@gmail.com>
2022-10-10 08:58:27 +0100

    tidy up

--- src/main/java/com/philippe/app/domain/AddressEventMessage.java
+++ src/main/java/com/philippe/app/domain/AddressEventMessage.java
@@ -3,9 +3,11 @@ package com.philippe.app.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class AddressEventMessage {
     private String eventType;
-    private Object data;    // TODO
+    private Map<String, String> data;
 }
